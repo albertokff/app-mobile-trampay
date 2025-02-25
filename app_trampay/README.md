54c92ca9104ad1207cedc180d6b837f79fc12aee
Diogo  Alberto <kirchoffalberto@gmail.com>
2025-02-24 22:13:36 -0300

    ✨feat: commit initial

---
+++ app_trampay/README.md
@@ -0,0 +1,3 @@
+# app_trampay
+
+A new Flutter project.
---
+++ app_trampay/android/app/src/main/kotlin/com/example/app_trampay/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_trampay
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
